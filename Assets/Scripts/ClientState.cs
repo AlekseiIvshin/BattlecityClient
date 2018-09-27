086dd75aa6ae55e6b237c4fac7fb199ea1d28374
Aleksei <eficksan@gmail.com>
2018-09-28 01:12:50 +0400

    Added Startup scene

---
+++ Assets/Scripts/ClientState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class ClientState 
+{
+    public static string serverAddress;
+    public static float tickTime;
+
+
+    public static string KEYS_URL = "http://codenjoy.juja.com.ua/codenjoy-contest/rest/sprites/battlecity";
+    public static string DEFAULT_SERVER_URL = "ws://codenjoy.juja.com.ua/codenjoy-contest/screen-ws?user=test@test.com&code=20998118591535248716";
+    public static string MESSAGE = "{name: 'getScreen', allPlayersScreen: true, gameName:'battlecity'}";
+}
---
+++ Assets/Scripts/UI/StartupControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class StartupControl : MonoBehaviour
+{
+    private string _serverAddress = "";
+
+    public void onTextChanged(string nextText)
+    {
+        _serverAddress = nextText;
+    }
+
+    public void onStartConnect()
+    {
+        if (_serverAddress.Length ==0)
+        {
+            ClientState.serverAddress = ClientState.DEFAULT_SERVER_URL;
+        } else
+        {
+            ClientState.serverAddress = _serverAddress;
+        }
+        SceneManager.LoadScene("_Complete-Game");
+    }
+}
